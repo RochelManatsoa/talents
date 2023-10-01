439de4da19d6c56b82a20ecb3a217cc0f5687e75
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-10-01 19:06:14 +0300

    add bootstrat icon

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -91,6 +91,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_home_terms');
+        return $this->redirectToRoute('app_connect');
     }
 }
